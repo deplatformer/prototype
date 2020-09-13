2b6c637c3e1e1c023c8db447ca38f9f4a0dbd394
Peter Van Garderen <peter@artefactual.com>
2020-09-13 15:23:35 -0700

    enable and customize login accounts

--- config.py
+++ config.py
@@ -13,16 +13,18 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False    # Avoids SQLAlchemy warning
 
 # Flask-User settings
 # See https://flask-user.readthedocs.io/en/latest/configuring_settings.html
-USER_APP_NAME = "Deplatformr"  # Shown in and email templates and page footers
+# Shown in and email templates and page footers
+USER_APP_NAME = "Deplatformr - Prototype"
 USER_ENABLE_USERNAME = True    # Enable username authentication
 USER_ENABLE_EMAIL = False      # Disable email authentication
 USER_ENABLE_CONFIRM_EMAIL = False
 USER_REQUIRE_RETYPE_PASSWORD = True
 USER_ENABLE_CHANGE_USERNAME = False
 USER_ENABLE_CHANGE_PASSWORD = True
-USER_ENABLE_REMEMBER_ME = True
+USER_ENABLE_REMEMBER_ME = False
 USER_AUTO_LOGIN = True
 USER_AUTO_LOGIN_AFTER_CONFIRM = True
-USER_AUTO_LOGIN_AFTER_REGISTER = False
-USER_AUTO_LOGIN_AFTER_RESET_PASSWORD = False
+USER_AUTO_LOGIN_AFTER_REGISTER = True
+USER_AUTO_LOGIN_AFTER_RESET_PASSWORD = True
 USER_AUTO_LOGIN_AT_LOGIN = True
+USER_AFTER_LOGOUT_ENDPOINT = "user.login"
--- deplatformr/views.py
+++ deplatformr/views.py
@@ -1,32 +1,20 @@
-from flask import Flask, render_template, render_template_string
+from flask import Flask, render_template, render_template_string, redirect
 from flask_user import login_required
 from deplatformr import app
 
 
-# The Home page is accessible to anyone
 @app.route('/')
+@login_required
 def home_page():
-    # String-based templates
-    return render_template_string("""
-        {% extends "flask_user_layout.html" %}
-        {% block content %}
-            <h2>Home page</h2>
-            <p><a href={{ url_for('user.register') }}>Register</a></p>
-            <p><a href={{ url_for('user.login') }}>Sign in</a></p>
-            <p><a href={{ url_for('home_page') }}>Home page</a> (accessible to anyone)</p>
-            <p><a href={{ url_for('member_page') }}>Member page</a> (login required)</p>
-            <p><a href={{ url_for('user.logout') }}>Sign out</a></p>
-        {% endblock %}
-        """)
+    return render_template("index.html")
 
 
-# The Members page is only accessible to authenticated users via the @login_required decorator
 @app.route('/members')
 @login_required    # User must be authenticated
 def member_page():
     # String-based templates
     return render_template_string("""
-        {% extends "flask_user_layout.html" %}
+        {% extends "base.html" %}
         {% block content %}
             <h2>Members page</h2>
             <p><a href={{ url_for('user.register') }}>Register</a></p>
