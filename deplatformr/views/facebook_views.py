625fbaf63d9879d5567eb6369a0db9b84972fb10
Peter Van Garderen <peter@artefactual.com>
2020-09-20 12:06:09 -0700

    move breadcrumbs and flash alert

--- deplatformr/views/facebook_views.py
+++ deplatformr/views/facebook_views.py
@@ -10,7 +10,7 @@ from deplatformr.helpers.facebook_helpers import posts_to_db
 @app.route('/facebook-deplatform')
 @login_required
 def facebook_deplatform():
-    return render_template("facebook/facebook-deplatform.html")
+    return render_template("facebook/facebook-deplatform.html", breadcrumb="Facebook / Deplatform")
 
 
 @app.route('/facebook-upload', methods=["GET", "POST"])
@@ -39,7 +39,7 @@ def facebook_upload():
             os.makedirs(facebook_dir)
 
         # Save the uploaded file
-        # TODO: move to background worker task (e.g. Celery)
+        # TODO: move to background worker task
         file_name = secure_filename(upload.filename)
         print("Saving uploaded file")  # TODO: move to async user output
         try:
@@ -48,37 +48,40 @@ def facebook_upload():
             # Return if the user did not provide a file to upload
             upload_stats = ""  # query to retrieve all current and historical upload stats
             # TODO: Add flash output to facebook_upload template
-            flash("Please choose a .zip file to upload to Deplatformr")
+            flash("Please choose a .zip file to upload to Deplatformr", "alert-danger")
             return render_template(
                 "facebook/facebook-upload.html", upload_stats=upload_stats
             )
 
         # Unzip the uploaded file
-        # TODO: move to background worker task (e.g. Celery)
+        # TODO: move to background worker task
         print("Extracting zip file")  # TODO: move to async user output
         try:
             unzip_dir = unzip(os.path.join(facebook_dir, file_name))
         except:
             upload_stats = ""  # query to retrieve all current and historical upload stats
-            flash("Unable to extract zip file.")
+            flash("Unable to extract zip file.", "alert-danger")
             return render_template(
                 "facebook/facebook-upload.html", upload_stats=upload_stats
             )
 
         # Parse Facebook JSON and save to SQLite
-        # TODO: move to background worker task (e.g. Celery)
+        # TODO: move to background worker task
         try:
-            print("Parsing Facebook content.") # TODO: move to async user output
-            print("Saving posts to database.") # TODO: move to async user output
+            # TODO: move to async user output
+            print("Parsing Facebook content.")
+            # TODO: move to async user output
+            print("Saving posts to database.")
             total_posts, max_date, min_date, profile_updates, total_media = posts_to_db(
                 unzip_dir)
             print(str(total_posts[0]) + " posts between " +
                   min_date[0] + " and " + max_date[0] + " were saved.")
             print("Including " + str(profile_updates) + " profile updates.")
-            print(str(total_media[0]) + " media files were linked.") # TODO: move to async user output
+            # TODO: move to async user output
+            print(str(total_media[0]) + " media files were linked.")
         except Exception as e:
             upload_stats = ""  # query to retrieve all current and historical upload stats
-            flash(e)
+            flash(e, "alert-danger")
             return render_template(
                 "facebook/facebook-upload.html", upload_stats=upload_stats)
 
@@ -88,16 +91,16 @@ def facebook_upload():
 
     upload_stats = ""  # query to retrieve all current and historical upload stats
 
-    return render_template("facebook/facebook-upload.html", upload_stats=upload_stats)
+    return render_template("facebook/facebook-upload.html", upload_stats=upload_stats, breadcrumb="Facebook / Upload content")
 
 
 @ app.route('/facebook-view')
 @ login_required
 def facebook_view():
-    return render_template("facebook/facebook-view.html")
+    return render_template("facebook/facebook-view.html", breadcrumb="Facebook / View content")
 
 
 @ app.route('/facebook-manage')
 @ login_required
 def facebook_manage():
-    return render_template("facebook/facebook-manage.html")
+    return render_template("facebook/facebook-manage.html", breadcrumb="Facebook / Manage content")
--- deplatformr/views/views.py
+++ deplatformr/views/views.py
@@ -12,34 +12,34 @@ def homepage():
 @app.route('/filecoin-files')
 @login_required
 def filecoin_files():
-    return render_template("filecoin/filecoin-files.html")
+    return render_template("filecoin/filecoin-files.html", breadcrumb="Filecoin / Files")
 
 
 @app.route('/filecoin-wallets')
 @login_required
 def filecoin_wallets():
-    return render_template("filecoin/filecoin-wallets.html")
+    return render_template("filecoin/filecoin-wallets.html", breadcrumb="Filecoin / Wallets")
 
 
 @app.route('/filecoin-deals')
 @login_required
 def filecoin_deals():
-    return render_template("filecoin/filecoin-deals.html")
+    return render_template("filecoin/filecoin-deals.html", breadcrumb="Filecoin / Deals")
 
 
 @app.route('/instagram')
 @login_required
 def instagram():
-    return render_template("instagram/instagram.html")
+    return render_template("instagram/instagram.html", breadcrumb="Instagram")
 
 
 @app.route('/icloud')
 @login_required
 def icloud():
-    return render_template("icloud/icloud.html")
+    return render_template("icloud/icloud.html", breadcrumb="iCloud")
 
 
 @app.route('/google')
 @login_required
 def google():
-    return render_template("google/google.html")
+    return render_template("google/google.html", breadcrumb="Google")
