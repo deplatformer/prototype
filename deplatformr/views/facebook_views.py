d6a1dd605e5d560c743905979c5067bf61c7f4fb
Peter Van Garderen <peter@artefactual.com>
2020-09-29 17:09:06 -0700

    use sqlite file extension instead of db

--- deplatformr/views/facebook_views.py
+++ deplatformr/views/facebook_views.py
@@ -151,7 +151,7 @@ def facebook_view():
 
     fb_dir = directory[0]
     db_name = os.path.basename(os.path.normpath(fb_dir))
-    fb_db = fb_dir + "/" + str(db_name) + ".db"
+    fb_db = fb_dir + "/" + str(db_name) + ".sqlite"
 
     facebook_db = sqlite3.connect(fb_db)
     cursor = facebook_db.cursor()
@@ -199,7 +199,7 @@ def facebook_memories():
 
     fb_dir = directory[0]
     db_name = os.path.basename(os.path.normpath(fb_dir))
-    fb_db = fb_dir + "/" + str(db_name) + ".db"
+    fb_db = fb_dir + "/" + str(db_name) + ".sqlite"
 
     day = datetime.now().strftime("%d")
     month = datetime.now().strftime("%m")
@@ -324,7 +324,7 @@ def facebook_album(album_id):
 
     fb_dir = directory[0]
     db_name = os.path.basename(os.path.normpath(fb_dir))
-    fb_db = fb_dir + "/" + str(db_name) + ".db"
+    fb_db = fb_dir + "/" + str(db_name) + "sqlite"
 
     facebook_db = sqlite3.connect(fb_db)
     cursor = facebook_db.cursor()
